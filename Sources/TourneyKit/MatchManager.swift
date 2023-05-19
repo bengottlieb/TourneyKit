320cd48a14ddf8339bfe9d8f6892ca363b2d801d
Ben Gottlieb <ben@standalone.com>
2023-05-19 18:06:27 -0500

    first commit

---
+++ Sources/TourneyKit/MatchManager.swift
@@ -0,0 +1,8 @@
+//
+//  MatchManager.swift
+//  
+//
+//  Created by Ben Gottlieb on 5/19/23.
+//
+
+import Foundation
---
+++ TourneyKitTestHarness/TourneyKitTestHarnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourneyKitTestHarnessApp.swift
+//  TourneyKitTestHarness
+//
+//  Created by Ben Gottlieb on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TourneyKitTestHarnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
