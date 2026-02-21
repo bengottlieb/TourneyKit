1ed9dcbb7944e6edd9bf8e9054a67f1ef08fba5b
Ben Gottlieb <ben@standalone.com>
2026-02-21 15:54:15 -0600

    updated requirements

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
 		.package(url: "https://github.com/ios-tooling/CrossPlatformKit.git", from: "1.0.12"),
-		.package(path: "../JohnnyCache"),
+		.package(url: "https://github.com/ios-tooling/JohnnyCache.git", from: "1.0.9"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
